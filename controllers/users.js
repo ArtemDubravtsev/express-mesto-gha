ab7c8a336bfabd7cd8a1376e4b3f5e61ff636926
Artem Dubravtsev <a.dubravtsev@yandex.ru>
2023-10-16 17:55:21 +0300

    ПР13 ревью 1

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,24 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 module.exports.addUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.status(201).send(user))
-    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400).send({ message: "Переданы некорректные данные" });
+      } else {
+        res.status(500).send({ message: "На сервере произошла ошибка" });
+      }
+    });
 };
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+    .catch(() =>
+      res.status(500).send({ message: "На сервере произошла ошибка" })
+    );
 };
 
 module.exports.getUserById = (req, res) => {
@@ -18,14 +26,14 @@ module.exports.getUserById = (req, res) => {
     User.findById(req.params.userId)
       .then((user) => {
         if (!user) {
-          res.status(404).send({ message: 'Пользователь не найден' });
+          res.status(404).send({ message: "Пользователь не найден" });
           return;
         }
         res.send(user);
       })
-      .catch(() => res.status(404).send({ message: 'Пользователь не найден' }));
+      .catch(() => res.status(404).send({ message: "Пользователь не найден" }));
   } else {
-    res.status(400).send({ message: 'Переданы некорректные данные' });
+    res.status(400).send({ message: "Переданы некорректные данные" });
   }
 };
 
@@ -34,9 +42,9 @@ module.exports.editUserData = (req, res) => {
   if (req.user._id) {
     User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
       .then((user) => res.send(user))
-      .catch(() => res.status(404).send({ message: 'Пользователь не найден' }));
+      .catch(() => res.status(404).send({ message: "Пользователь не найден" }));
   } else {
-    res.status(500).send({ message: 'На сервере произошла ошибка' });
+    res.status(500).send({ message: "На сервере произошла ошибка" });
   }
 };
 
@@ -45,11 +53,11 @@ module.exports.editUserAvatar = (req, res) => {
     User.findByIdAndUpdate(
       req.user._id,
       { avatar: req.body.avatar },
-      { new: true },
+      { new: true }
     )
       .then((user) => res.send(user))
-      .catch(() => res.status(404).send({ message: 'Пользователь не найден' }));
+      .catch(() => res.status(404).send({ message: "Пользователь не найден" }));
   } else {
-    res.status(500).send({ message: 'На сервере произошла ошибка' });
+    res.status(500).send({ message: "На сервере произошла ошибка" });
   }
 };
