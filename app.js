4882b95dbcfc9039b1c6a35e4a1fedd5f4ecd01e
Artem Dubravtsev <a.dubravtsev@yandex.ru>
2023-10-30 12:33:32 +0200

    ПР14 ревью 2, автотест

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const NotFoundError = require('./errors/NotFoundError');
@@ -7,6 +9,14 @@ const NotFoundError = require('./errors/NotFoundError');
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
+
+app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
